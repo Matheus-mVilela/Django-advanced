df22314dc5883e8a94c3aa5900481e8d3fc84dcb
Matheus Vilela <matheus_vilela@icloud.com>
2020-09-03 07:15:10 -0300

    TemplateView Class Based View

--- gestao-clientes/home/views.py
+++ gestao-clientes/home/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.views.generic.base import TemplateView
+from django.utils import timezone
+from django.views.generic.detail import DetailView
 
 
 def home(request):
-    import pdb; 
-    pdb.set_trace()
-    a=10
-    b=20
-    c=a*10
-    return render(request, 'home.html', {'result':c})
+    return render(request, 'home.html')
 
 
 def my_logout(request):
     logout(request)
     return redirect('home')
+
+
+class HomePageView(TemplateView):
+    template_name= 'home3.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
