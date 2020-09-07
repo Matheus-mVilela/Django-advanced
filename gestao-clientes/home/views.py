9aaaff1ef1b5408c529790cad3d8b7c331b3cb50
Matheus Vilela <matheus_vilela@icloud.com>
2020-09-07 09:32:53 -0300

    view the simplest, method get and post

--- gestao-clientes/home/views.py
+++ gestao-clientes/home/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import logout
 from django.views.generic.base import TemplateView
 from django.utils import timezone
 from django.views.generic.detail import DetailView
+from django.http import HttpResponse
+from django.views import View
 
 
 def home(request):
@@ -20,4 +22,15 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
-        return context
+        return context     
+
+
+class MyView(View):
+
+    def get(self, request, *args, **kwargs):
+     return render(request, 'home3.html')
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('Teste')
+
+         
