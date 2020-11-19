dc2f3088a5d169def8e22926b89462d1bd283dea
Matheus_Vilela <matheus_vilela@icloud.com>
2020-11-19 07:25:15 -0300

    dajngo admin class admin

--- gestao-clientes/clientes/admin.py
+++ gestao-clientes/clientes/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 from .models import Person, Documento, Venda, Produto
 
 
-admin.site.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    # fields = ("last_name","age","salary","bio","photo","doc",)
+    exclude = ("bio",)
+    list_display = (
+        "first_name",
+        "last_name",
+        "age",
+        "salary",
+        "bio",
+        "photo",
+        "doc",
+    )
+
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)
 admin.site.register(Venda)
 admin.site.register(Produto)
