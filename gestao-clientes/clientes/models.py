1f3b6027b9f0e34c3b116485371e7840550aef91
Matheus_Vilela <matheus_vilela@icloud.com>
2020-11-25 07:36:38 -0300

    Metodos personalizados para

--- gestao-clientes/clientes/models.py
+++ gestao-clientes/clientes/models.py
@@ -15,7 +15,7 @@ class Person(models.Model):
     salary = models.DecimalField(max_digits=5, decimal_places=2)
     bio = models.TextField()
     photo = models.ImageField(
-        upload_to="clients_photos", null=True, blank=True
+        upload_to='clients_photos', null=True, blank=True
     )
     doc = models.OneToOneField(
         Documento, null=True, blank=True, on_delete=models.CASCADE
@@ -23,10 +23,10 @@ class Person(models.Model):
 
     @property
     def name_full(self):
-        return self.first_name + " " + self.last_name
+        return self.first_name + ' ' + self.last_name
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return self.first_name + ' ' + self.last_name
 
 
 class Produto(models.Model):
@@ -46,6 +46,13 @@ class Venda(models.Model):
         Person, null=True, blank=True, on_delete=models.PROTECT
     )
     produtos = models.ManyToManyField(Produto, blank=True)
+
+    def get_total(self):
+        tot = 0
+        for produto in self.produtos.all():
+            tot += produto.preco
+
+        return (tot - self.desconto) - self.impostos
 
     def __str__(self):
         return self.numero
