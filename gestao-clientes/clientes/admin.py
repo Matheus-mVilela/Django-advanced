eb48f88f3288e5671a2455a820b409bc8815ec17
Matheus_Vilela <matheus_vilela@icloud.com>
2020-12-05 16:02:39 -0300

    filter_vertical , filter_horizontal  - produtos

--- gestao-clientes/clientes/admin.py
+++ gestao-clientes/clientes/admin.py
@@ -55,6 +55,10 @@ class VendaAdmin(admin.ModelAdmin):
     search_fields = ('id', 'pessoa__first_name', 'pessoa__doc__num_doc')
     row_id_fields = ('pessoa',)
     actions = [nfe_emitida, nfe_nao_emitida]
+    # filter_vertical = ['produtos',]
+    filter_horizontal = [
+        'produtos',
+    ]
 
 
 class ProdutoAdmin(admin.ModelAdmin):
