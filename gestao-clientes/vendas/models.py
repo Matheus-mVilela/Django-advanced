edc32696dfae83eab91254737161c33f9933df52
Matheus_Vilela <matheus_vilela@icloud.com>
2020-12-13 21:19:39 -0300

    criando @property para calculo valor venda com Sum

--- gestao-clientes/vendas/models.py
+++ gestao-clientes/vendas/models.py
@@ -3,17 +3,31 @@ from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 from clientes.models import Person
 from produtos.models import Produto
+from django.db.models import Sum, F, FloatField, Max, Aggregate
 
 
 class Venda(models.Model):
-    numero = models.CharField(max_length=7)
-    valor = models.DecimalField(max_digits=5, decimal_places=2)
-    desconto = models.DecimalField(max_digits=5, decimal_places=2)
-    impostos = models.DecimalField(max_digits=5, decimal_places=2)
+    desconto = models.FloatField()
+    impostos = models.FloatField()
     pessoa = models.ForeignKey(
         Person, null=True, blank=True, on_delete=models.PROTECT
     )
     nfe_emitida = models.BooleanField(default=False)
+
+    @property
+    def valor(self):
+
+        tot = self.itendopedido_set.all().aggregate(
+            tot_ped=Sum(
+                (F('quantidade') * F('produto__preco')) - F('desconto'),
+                output_field=FloatField(),
+            )
+        )['tot_ped']
+
+        if tot:
+            return tot - self.desconto - self.impostos
+        else:
+            return 0
 
     # @property
     # def valor(self):
@@ -26,20 +40,22 @@ class Venda(models.Model):
     def __str__(self):
         return self.numero
 
-    def save(self, *args, **kwargs):
-        self.valor = 0
-        super(Venda, self).save(*args, **kwargs)
-        for produto in self.produtos.all():
-            self.valor += produto.preco
+        # def save(self, *args, **kwargs):
+        #   self.valor = 0
+        #  super(Venda, self).save(*args, **kwargs)
+        # for produto in self.produtos.all():
+        #    self.valor += produto.preco
 
-        self.valor = (self.valor - self.desconto) - self.impostos
+        # self.valor = (self.valor - self.desconto) - self.impostos
 
-        return super(Venda, self).save(*args, **kwargs)
+    # return super(Venda, self).save(*args, **kwargs)
 
 
 class ItenDoPedido(models.Model):
     venda = models.ForeignKey(Venda, on_delete=models.CASCADE)
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    quantidade = models.BooleanField()
+    quantidade = models.FloatField()
     desconto = models.DecimalField(max_digits=5, decimal_places=2)
 
+    def __str__(self):
+        return f'{self.venda.pk}-{self.produto.descricao}'
