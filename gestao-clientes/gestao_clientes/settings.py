7491b56a0c59f6374d84b025d881b12154f7621d
Matheus_Vilela <matheus_vilela@icloud.com>
2020-12-31 07:49:52 -0300

    criando mymiddlewares

--- gestao-clientes/gestao_clientes/settings.py
+++ gestao-clientes/gestao_clientes/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'gestao_clientes.mymiddlewares.metadata.MetaData',
 ]
 
 ROOT_URLCONF = 'gestao_clientes.urls'
